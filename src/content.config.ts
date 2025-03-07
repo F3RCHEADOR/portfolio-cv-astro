4ae5b17fbc9b648a23a47f8acda3224cd8460085
Fabian F <noobseca123@hotmail.com>
2025-03-06 22:00:25 -0500

    env agregados

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,20 +1,6 @@
 import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
-});
-
 const project = defineCollection({
 	loader: glob({ base: './src/content/project', pattern: '**/*.{md,mdx}' }),
 	schema: z.object({
@@ -31,4 +17,4 @@ const project = defineCollection({
 	})
 })
 
-export const collections = { blog, project };
+export const collections = { project };
